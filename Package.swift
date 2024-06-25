264e67424e3f0a9e5f1a56c3c17d3e2c223b1ae2
kaiba42 <50125442+kaiba42@users.noreply.github.com>
2024-06-24 17:44:20 -0700

    Fix just build for sdk-ios (#897)
    * feat(monorepo): add just nixRoot command for tracing root sources of dependencies
    
    * build(sdk-ios): fixes just build within devenv by removing bad environment from devenv
    
    * build(sdk-ios): fix bad rm command
    
    * chore(sdk-ios): publish v0.3.1 xcframework
    
    * chore(sdk-ios): bump version for swift package and cocoapod to v0.3.1
    
    * feat(sdk-ios): add support for publishing to other branches in the sdk-ios-pkg repo
    
    * refactor(monorepo): move unsetEnvVars to separate file

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // .binaryTarget(name: "AnonKit", path: "./Sources/AnonKit.xcframework"),
         .binaryTarget(
             name: "AnonKit", 
-            url: "https://dl.cloudsmith.io/4GEEbXtn1Z9TOSLs/anon/anon-sdk/raw/names/AnonKit.xcframework/versions/0.2.0/AnonKit.xcframework.zip",
-            checksum: "b0e11b5fff8437feb5f6f90fb1033ce795df88f9d92a833ea71609ca32f00472"
+            url: "https://dl.cloudsmith.io/bFQP6zY5UO2hAJGx/anon/anon-sdk/raw/names/AnonKit.xcframework/versions/0.3.1/AnonKit.xcframework.zip",
+            checksum: "06725f968787677efe4ee945de3e9e303b6820c7d93389343b323d4ab510e546"
         ),
         .testTarget(
             name: "AnonKitPkgTests",
